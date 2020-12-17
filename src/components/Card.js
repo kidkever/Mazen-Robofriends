47d4877143f4af408030610f69eee526069304b6
Mazen Mohamed <kidkever@yahoo.com>
2020-12-17 03:07:12 +0200

    pwa lighthouse updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,11 @@ import React from "react";
 const Card = ({ id, name, email }) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
+      <img
+        src={`https://robohash.org/${id}?size=525x525`}
+        alt="robots"
+        style={{ width: "200px", height: "200px" }}
+      />
       <div>
         <h2 className="mt2 mb1">{name}</h2>
         <p className="mt1 mb1">{email}</p>
