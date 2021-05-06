3684fe9e94733eed3dc18a2e19e0c2a9f55b23ce
Mazen Mohamed <kidkever@yahoo.com>
2021-05-07 00:48:35 +0200

    prettier again

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -34,7 +34,7 @@ class MainPage extends Component<Props> {
   render() {
     const { onSearchChange, robots, isPending } = this.props;
     if (isPending) {
-      return <h1 className="tc">Loading</h1>;
+      return <h2 className="tc">Loading</h2>;
     } else {
       return (
         <div className="tc">
